337f841c0684280f8a10023dbf50c1e9b5c2337c
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2022-09-16 16:37:30 +0200

    Add main screen

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,12 @@
+import React, { Component, ReactNode } from "react";
+import { Text, View } from "react-native";
+
+export default class MainScreen extends Component {
+    public render(): ReactNode {
+        return (
+            <View>
+                <Text>Main Screen</Text>
+            </View>
+        )
+    }
+}
